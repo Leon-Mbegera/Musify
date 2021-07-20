467b4803f42e7206db44d43ef327b6a08d40095a
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-07-20 13:22:12 +0300

    Add unit tests for user model

---
+++ spec/model_tests/user_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:user) { User.create(name: 'leon', email: 'leon@example', password: '123456')}
+
+  it 'ensures a user has a name' do
+    user = User.create(email: 'leon@example', password: '123456')
+    expect(user.valid?).to be false
+  end
+
+  it 'ensures a user has an email' do
+    user = User.create(name: 'leon', password: '123456')
+    expect(user.valid?).to be false
+  end
+
+  it 'ensures a user has an password' do
+    user = User.create(name: 'leon', email: 'leon@example',)
+    expect(user.valid?).to be false
+  end
+
+  it 'ensures a user can have zero or many articles' do
+    user = User.reflect_on_association(:articles)
+    expect(user.macro).to eq(:has_many)
+  end
+
+  it 'ensure a user can have zero or many comments' do
+    user = User.reflect_on_association(:comments)
+    expect(user.macro).to eq(:has_many)
+  end
+end
