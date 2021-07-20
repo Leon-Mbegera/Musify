8455c6ba43dd9b27fd6b6b5a8def9b2cc4af3d4d
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-07-20 13:49:16 +0300

    Add tests for vote model

---
+++ spec/model_tests/vote_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Vote, type: :model do
+
+  it 'should have both user_id and article_id present ' do
+    vote = Vote.new(user_id: 1)
+    expect(vote.save).to be false
+  end
+
+  it 'should belong to a user' do
+    vote = Vote.reflect_on_association(:user)
+    expect(vote.macro).to eq(:belongs_to)
+  end
+
+  it 'should belong to an article' do
+    vote = Vote.reflect_on_association(:article)
+    expect(vote.macro).to eq(:belongs_to)
+  end
+end
