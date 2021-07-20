c62576416d2feffd9dd4846c68181618e9e5de0f
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-07-20 15:18:34 +0300

    Modify associations in model files

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, length: { maximum: 25 }
+
+  has_many :articles
+  has_many :votes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
