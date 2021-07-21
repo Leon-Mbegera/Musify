0a72c45c86dd93810df724abe66b5af886bd1c9f
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-07-21 13:07:22 +0300

    Build action to create an article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,23 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    
+    @article = current_user.articles.build(article_params)
+    if @article.image.nil?
+      @article.image = 'default.jpg'
+    end
+
+    if @article.save
+      redirect_to article_path(@article_id)
+      notice: 'Your article has been created!'
+    else
+      render :new
+      notice: 'Article creation failed!'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:category_id, :title, :body, :image)
   end
 end
