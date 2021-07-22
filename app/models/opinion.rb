b4b93fd3c2f053ffb3635c95a1fcc49e8c6d2c98
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-07-22 16:20:01 +0300

    Add unit tests for opinion model

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -2,5 +2,4 @@ class Opinion < ApplicationRecord
   belongs_to :user
   belongs_to :article
   validates :content, presence: true, length: { maximum: 50, too_long: "Opinions can't exceed 50 characters"}
-
 end
---
+++ spec/model_tests/category_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+
+  it 'should have zero or many articles' do
+    first_category = Category.reflect_on_association(:articles)
+    expect(first_category.macro).to eq(:has_many)
+  end
+end
