39e403639ba05d7ae87e439c23d238d1f71f3a10
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-07-22 13:34:48 +0300

    Add associations between opinion and other models plus its validation

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,2 +1,6 @@
 class Opinion < ApplicationRecord
+  belongs_to :user
+  belongs_to :article
+  validates :content, presence: true, length: { maximum: 50, too_long: "Opinions can't exceed 50 characters"}
+
 end
