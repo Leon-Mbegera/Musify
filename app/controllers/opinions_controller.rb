e8a58973a30d32cb2f9e82acefa606da48e66ce7
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-07-25 16:46:17 +0300

    Write create action to build an opinion

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,2 +1,20 @@
 class OpinionsController < ApplicationController
+
+  def create
+    @opinion = Opinion.new(opinion_params)
+    @opinion.user = current_user
+    @opinion.article = params[:article_id]
+
+    if @opinion.save
+      redirect_to articles_path, notice: 'Opinion placed!'
+    else
+      redirect_to articles_path, notice: 'Opinion could not be placed'
+    end
+  end
+
+  private
+
+  def opinion_params
+    params.require(:opinion).permit(:content)
+  end
 end
