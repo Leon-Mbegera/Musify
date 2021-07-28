9cac4beb9e99a1ff5015aed86c85e42f12de9b63
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-07-28 13:52:47 +0300

    Remove controversial image validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,6 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50,
                                               too_long: "Title can't exceed 50 characters long." }
   validates :body, presence: true, length: { maximum: 150, too_long: "Body can't exceed 150 characters long." }
-  validates :image, presence: true
   validates :category_id, presence: true
 
   belongs_to :user
